0277c93303f80b56312ccbb739a39356a9875042
Yasuo Ashina <ashipp3000@gmail.com>
2020-12-29 09:04:03 +0900

    new project

---
+++ DQ3/DQ3App.swift
@@ -0,0 +1,17 @@
+//
+//  DQ3App.swift
+//  DQ3
+//
+//  Created by aship on 2020/12/29.
+//
+
+import SwiftUI
+
+@main
+struct DQ3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
