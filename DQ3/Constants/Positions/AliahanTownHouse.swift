4815e162c0f6aabe2c354dbdcd6a4316342658bb
Yasuo Ashina <ashipp3000@gmail.com>
2021-01-19 22:37:48 +0900

    bumping version 0.7 check NPC collision

--- DQ3/Constants/Positions/AliahanTownHouse.swift
+++ DQ3/Constants/Positions/AliahanTownHouse.swift
@@ -9,6 +9,10 @@
 let AliahanTownHouseMotherPositionX = 4
 let AliahanTownHouseMotherPositionY = 5
 
+// 母がオープニングでさあ行きましょうと待機してる座標
+let AliahanTownHouseMotherWaitingPositionX = 8
+let AliahanTownHouseMotherWaitingPositionY = 2
+
 // 勇者がオープニングで母にさあ行きましょうと言われる座標
 let AliahanTownHouseMotherCallPositionX = 7
 
--- DQ3/Scenes/DQScene/DQMapScene/AliahanTown/AliahanTownSceneEvent.swift
+++ DQ3/Scenes/DQScene/DQMapScene/AliahanTown/AliahanTownSceneEvent.swift
@@ -49,6 +49,10 @@ extension AliahanTownScene {
         
         let motherNode = self.characterNpcNodes.first!
         
+        // どうせ移動するから最終ポジションに座標をセット
+        motherNode.positionX = AliahanTownMotherWaitingPositionX
+        motherNode.positionY = AliahanTownMotherWaitingPositionY
+        
         let actionsMother = SKAction.sequence([actionWaitAll,
                                                actionWait])
         
