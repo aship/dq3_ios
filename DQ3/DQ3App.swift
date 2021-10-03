662425e54275589a669adef4409312d84424444e
Yasuo Ashina <ashipp3000@gmail.com>
2021-10-03 19:11:15 +0900

    new project

---
+++ DQ3/DQ3App.swift
@@ -0,0 +1,17 @@
+//
+//  DQ3App.swift
+//  DQ3
+//
+//  Created by aship on 2021/10/03.
+//
+
+import SwiftUI
+
+@main
+struct DQ3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DQ3UITests/DQ3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DQ3UITestsLaunchTests.swift
+//  DQ3UITests
+//
+//  Created by aship on 2021/10/03.
+//
+
+import XCTest
+
+class DQ3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
