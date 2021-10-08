7b15bd98552344724ec6479e848eae35c1e78616
Yasuo Ashina <ashipp3000@gmail.com>
2021-10-08 13:54:47 +0900

    bumping version 0.12 NPC move

--- DQ3/Scenes/DQScene/DQMapScene/AliahanTownHouse/AliahanTownHouseScene.swift
+++ DQ3/Scenes/DQScene/DQMapScene/AliahanTownHouse/AliahanTownHouseScene.swift
@@ -58,6 +58,12 @@ class AliahanTownHouseScene: DQMapScene {
                          scale: scale,
                          dqStory: dqStory)
         
+        setNpcMove(characterNodes: DataManager.adventureLog.partyCharacterNodes,
+                   characterNpcNodes: self.characterNpcNodes,
+                   tileMapNode: self.mainTileMapNode,
+                   scale: self.scene.scale,
+                   dqStory: dqStory)
+        
         if dqStory == .opening {
             let text1 = "＊「おはよう　えにくす。"
             let text2 = "　　もう　あさですよ。"
