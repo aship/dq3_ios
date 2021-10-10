4b5d81002f8bf091c538914547b1ebc339a2bdc0
Yasuo Ashina <ashipp3000@gmail.com>
2021-10-10 17:11:14 +0900

    ttt

---
+++ DQ3/Scenes/DQScene/Battle/BattleMessageWindow/BattleGoldWindow/BattleGoldWindowNodeButtonA.swift
@@ -0,0 +1,36 @@
+//
+//  BattleGoldWIndowNodeButton.swift
+//  DQ3
+//
+//  Created by aship on 2021/10/10.
+//
+
+import SpriteKit
+
+extension BattleGoldWindowNode {
+    func processButtonA() {
+        let scene = self.parent!.parent as! BaseScene
+        
+     //   scene.transitionScene()
+        
+        
+        if DataManager.dqSceneTypeFromBattle == .field {
+            var dqAudio: DQAudio = .field
+            
+            if scene.fieldMoveMode == .ship {
+                dqAudio = .ship
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                scene.transitionFromBattle(dqSceneType: .field,
+                                           dqAudio: dqAudio)
+            }
+        }
+        else if DataManager.dqSceneTypeFromBattle == .alefgard {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                scene.transitionFromBattle(dqSceneType: .alefgard,
+                                           dqAudio: .alefgard)
+            }
+        }
+    }
+}
