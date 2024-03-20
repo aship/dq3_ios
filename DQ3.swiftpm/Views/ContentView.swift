6dd4101f2fe4165212e91b56b24350cc73e2425d
Yasuo Ashina <ashipp3000@gmail.com>
2024-03-20 09:13:55 +0900

    bumping version 0.3 refactor with CharacterNode and DataManager

--- DQ3.swiftpm/Views/ContentView.swift
+++ DQ3.swiftpm/Views/ContentView.swift
@@ -1,9 +1,38 @@
+//
+//  ContentView.swift
+//  DQ3
+//
+//  Created by aship on 2020/12/08.
+//
+
 import SpriteKit
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         let scene = TitleScene()
+
+        let characterNode = CharacterNode(dqCharacter: .hero)
+
+        let adventureLog = AdventureLog()
+        adventureLog.heroName = "えにくす"
+        adventureLog.partyCharacterNodes.append(characterNode)
+
+        DataManager.adventureLog = adventureLog
+
+        // let scene = AliahanTownScene()
+        // for node in DataManager.adventureLog.partyCharacterNodes {
+        //     node.positionX = AliahanTownEntrancePositionX
+        //     node.positionY = AliahanTownEntrancePositionY
+        // }
+
+        // let scene = AliahanTownHouseScene()
+
+        // let scene = FieldScene()
+        // for node in DataManager.adventureLog.partyCharacterNodes {
+        //     node.positionX = FieldAliahanPositionX
+        //     node.positionY = FieldAliahanPositionY
+        // }
 
         return Group {
             SpriteView(scene: scene)
